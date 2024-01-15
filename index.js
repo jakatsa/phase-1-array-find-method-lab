607b3044571eaac141fc96c9307c198b31a74e33
Joe <jakatsa5@gmail.com>
2024-01-15 20:52:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const superbowlWin = (record) => {
+  const winningRecord = record.find((entry) => entry.result === "W");
+
+  return winningRecord ? winningRecord.year : undefined;
+};
+
+const winningYear = superbowlWin(record);
+console.log(winningYear);
